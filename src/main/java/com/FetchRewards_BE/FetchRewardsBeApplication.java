b3a6a5ea8195fb8a269b0bbcfee0e362d90e2ed8
Rishabh <rmehta35@uic.edu>
2021-05-25 11:52:10 -0500

    Initial commit

---
+++ src/main/java/com/FetchRewards_BE/FetchRewardsBeApplication.java
@@ -0,0 +1,13 @@
+package com.FetchRewards_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FetchRewardsBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FetchRewardsBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FetchRewards_BE/FetchRewardsBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FetchRewards_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FetchRewardsBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
